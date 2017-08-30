584951d4782abf8d4a6dd55735aa323611345682
Robert O'Rourke <rob@hmn.md>
2017-08-30 11:54:49 +0100

    updated to be backwards compatible

--- modules/openssl-nginx/manifests/init.pp
+++ modules/openssl-nginx/manifests/init.pp
@@ -1,4 +1,11 @@
 class openssl-nginx {
+	if ( ! defined( File["/etc/nginx/sites-available/${name}.d"] ) ) {
+		file { "/etc/nginx/sites-available/$fqdn.d":
+			ensure => directory,
+			require => Package['nginx']
+		}
+	}
+	
 	file { "/etc/nginx/sites-available/$fqdn.d/ssl":
 		content => template('openssl-nginx/site.nginx.conf.ssl.erb'),
 		require => File[ "/etc/nginx/sites-available/$fqdn.d" ],
