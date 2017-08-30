019a57ab2330b4323644449b69711175d14f420a
Robert O'Rourke <rob@hmn.md>
2017-08-30 11:55:03 +0100

    cs

--- modules/openssl-nginx/manifests/init.pp
+++ modules/openssl-nginx/manifests/init.pp
@@ -5,7 +5,7 @@ class openssl-nginx {
 			require => Package['nginx']
 		}
 	}
-	
+
 	file { "/etc/nginx/sites-available/$fqdn.d/ssl":
 		content => template('openssl-nginx/site.nginx.conf.ssl.erb'),
 		require => File[ "/etc/nginx/sites-available/$fqdn.d" ],
